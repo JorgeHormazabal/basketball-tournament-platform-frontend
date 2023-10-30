c344fe67495d00005c395b7ee251702ee650982d
Pablo Arias <pabloarias.ic@gmail.com>
2023-10-29 23:03:59 -0300

    fix socket api urlo

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  import.meta.env.NODE_ENV ===
-  "https://basketball-tournament-platform-backend.onrender.com/api/";
+const URL = "https://basketball-tournament-platform-backend.onrender.com/api/";
 
 //const URL = "http://localhost:3000/";
 
