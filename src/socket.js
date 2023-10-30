b224d7603834cce3cb2cee568d4678fa13e4299c
Pablo Arias <pabloarias.ic@gmail.com>
2023-10-29 22:27:02 -0300

    cambiar api socket

--- src/socket.js
+++ src/socket.js
@@ -2,9 +2,10 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  import.meta.env.NODE_ENV === "production"
-    ? undefined
-    : "http://localhost:3000/";
+  import.meta.env.NODE_ENV ===
+  "https://basketball-tournament-platform-backend.onrender.com/api/";
+
+//const URL = "http://localhost:3000/";
 
 export const socket = io(URL, {
   autoConnect: false,
