7cbb0ccda6554e967f8a631f24080c05f4596730
Pablo Arias <pabloarias.ic@gmail.com>
2023-10-29 22:31:13 -0300

    acceso temporal

--- src/pages/invitado/Home/Home.jsx
+++ src/pages/invitado/Home/Home.jsx
@@ -1,65 +1,79 @@
-import "./home.css"
-import { Link } from "react-router-dom";
-import { liga1, EquiposTabla } from "../Ligas/Ligas"
+import "./home.css";
+import { Link, useNavigate } from "react-router-dom";
+import { liga1, EquiposTabla } from "../Ligas/Ligas";
 import { encuentros, TablaPasados } from "../Pasados/Pasados";
 import { CardsProximos } from "../Proximos/Proximos";
 const encuentrosproximos = encuentros.slice(0, 2);
 const encuentrospasados = encuentros.slice(2, 6);
 
-export function Home(){
+export function Home() {
+  const navigate = useNavigate();
   return (
     <>
+      <button onClick={() => navigate("/tablero/3")}>TABLERO TEMPORAL</button>
+      <button onClick={() => navigate("/panel/3")}>PANEL TEMPORAL</button>
+      {/* HEADER  */}
 
-      {/* HEADER  */}  
+      <div className="Home">
+        <div className="tituloHome">
+          <h1 id="Primero">¡Bienvenido a Tromü!</h1>
+          <h1 id="Segundo">Ligas de Baloncesto</h1>
+        </div>
+      </div>
 
-     <div className="Home">
-      <div className="tituloHome">
-        <h1 id="Primero">¡Bienvenido a Tromü!</h1>
-        <h1 id="Segundo">Ligas de Baloncesto</h1>
-      </div>
-      </div>
-    
-      {/* PROXIMOS PARTIDOS  */}  
+      {/* PROXIMOS PARTIDOS  */}
 
       <div className="Proximos">
-      <h2 id="cuarto">Próximos Enfrentamientos</h2> 
-      <CardsProximos encuentros={encuentrosproximos} />
-      <br />
+        <h2 id="cuarto">Próximos Enfrentamientos</h2>
+        <CardsProximos encuentros={encuentrosproximos} />
+        <br />
       </div>
 
-
-      {/* PARTIDOS PASADOS  */} 
+      {/* PARTIDOS PASADOS  */}
       <div className="contenedorpasado">
-      <div className="Pasados">
-      <h2 id="cuarto">Últimos Enfrentamientos</h2> 
-      <TablaPasados encuentros={encuentrospasados} />
-      </div>
+        <div className="Pasados">
+          <h2 id="cuarto">Últimos Enfrentamientos</h2>
+          <TablaPasados encuentros={encuentrospasados} />
+        </div>
       </div>
       <br />
-      
 
-      {/* DIV LIGAS  */}  
+      {/* DIV LIGAS  */}
 
       <div className="Ligass">
-      <h2 id="actualizacionligauno">Última Actualización de <strong>Liga Uno - Femenina</strong></h2> 
-      <div className="LigasAPP">
-      <div className="contenedorDeLiga">
-        <EquiposTabla equipos={liga1.equipos} />
-      </div>
-    </div>
+        <h2 id="actualizacionligauno">
+          Última Actualización de <strong>Liga Uno - Femenina</strong>
+        </h2>
+        <div className="LigasAPP">
+          <div className="contenedorDeLiga">
+            <EquiposTabla equipos={liga1.equipos} />
+          </div>
+        </div>
       </div>
 
-      {/* DIV CONOCE MAS */}  
+      {/* DIV CONOCE MAS */}
 
       <div className="botonesConoce">
         <div>
-        <h1 id="tercero">¡Conoce más de Tromü!</h1>
+          <h1 id="tercero">¡Conoce más de Tromü!</h1>
         </div>
-      <div className="botonesConoceDentro">
-      <Link to="/ligas"><button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Todas las Ligas</button></Link>
-      <Link to="/clubes"><button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Todos los Clubes</button></Link>
-      </div>
+        <div className="botonesConoceDentro">
+          <Link to="/ligas">
+            <button
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              Todas las Ligas
+            </button>
+          </Link>
+          <Link to="/clubes">
+            <button
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              Todos los Clubes
+            </button>
+          </Link>
+        </div>
       </div>
     </>
-  )
+  );
 }
